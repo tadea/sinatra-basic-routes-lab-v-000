719b42e3c2a63fe6478b8368be958da5c59df178
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-06-12 22:59:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Tadea"
+  end
+
+  get '/hometown' do
+    "My hometown is Zagreb"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Five hours"
+  end
+
 end
